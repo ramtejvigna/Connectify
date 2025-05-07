61657822092cf0c3b1dfbb29a59a72c4b43d0219
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2025-05-07 21:03:47 +0530

    Updated the server logic on storing message in a database and added a new image to the client public folder

--- server/server.js
+++ server/server.js
@@ -3,21 +3,6 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { Server } from 'socket.io';
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
-
-const db = await open({
-    filename: 'chat.db',
-    driver: sqlite3.Database
-});
-
-await db.exec(`
-  CREATE TABLE IF NOT EXISTS messages (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      client_offset TEXT UNIQUE,
-      content TEXT
-  );
-`);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -57,38 +42,14 @@ io.on('connection', async (socket) => {
     });
 
     socket.on('message', (msg) => {
-        if (msg === 'Sare' || msg === 'ok' || msg === 'Sare ok' || msg === 'sare' || msg === 'Alage' || msg === 'alage') {
-            msg = `Goppa pagipodhi.....${msg} anta ${msg}`;
-        }
-        else if (msg === 'bye' || msg === 'Bye' || msg === 'BYE') {
-            msg = 'Bye enti bye....Neat ga cheppu';
-        }
-
-        let result = db.run(`INSERT OR IGNORE INTO messages (client_offset, content) VALUES (?, ?)`, [socket.id, msg]);
-
-
         // When a message is sent, the user is no longer typing
         if (typingUsers.has(socket.id)) {
             typingUsers.delete(socket.id);
             socket.broadcast.emit('typing', false, socket.id);
         }
 
-        io.emit('message', msg, socket.id, result.lastID);
+        io.emit('message', msg, socket.id);
     });
-
-    if (!socket.recovered) {
-        try {
-            await db.each('SELECT id, content FROM messages WHERE id > ?',
-                [socket.handshake.auth.serverOffset || 0],
-                (_err, row) => {
-                    socket.emit('message', row.content, row.id);
-                }
-            )
-        } catch (e) {
-            // something went wrong
-            console.error('Error fetching messages:', e);
-        }
-    }
 
     // Add typing event handler
     socket.on('typing', (isTyping) => {
